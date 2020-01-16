5aa5026954c9b614a502123afded433608547302
houa cha <itshoua@gmail.com>
2020-01-15 23:38:27 -0600

    made css file, functions to edit, and get data from api

--- MovieLibrary.js
+++ MovieLibrary.js
@@ -1,19 +1,20 @@
-(function ($) {
-    var table = $("#movieTable");
+var table = $("#movieTable");
 
-    function ajax(type, info, htmlMethod) {
-        $.ajax({
-            url: 'https://localhost:44330/api/movies',
-            dataType: 'json',
-            type: type,
-            contentType: 'application/json',
-            data: info,
-            success: htmlMethod,
-            error: function (jqXhr, textStatus, errorThrown) {
-                console.log(errorThrown);
-            }
-        });
-    }
+function ajax(url, type, info, htmlMethod) {
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        type: type,
+        contentType: 'application/json',
+        data: info,
+        success: htmlMethod,
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+}
+
+(function ($) {
 
     function processForm(e) {
         var movie = {
@@ -21,7 +22,7 @@
             Genre: this["genre"].value,
             Director: this["director"].value
         };
-        ajax('post', JSON.stringify(movie),
+        ajax('https://localhost:44330/api/movies/', 'post', JSON.stringify(movie),
 
             function (data, textStatus, jQxhr) {
                 table.append(
@@ -33,20 +34,33 @@
         e.preventDefault();
     }
     $('#my-form').submit(processForm);
-
-    function makeTable() {
-        ajax('get', null,
-
-            function (data, textStatus, jQxhr) {
-                $.each(data,
-                    
-                    function (i,value) {
-                        table.append('<tr><td>' + value.Title + '</td><td>' + value.Genre + '</td><td>' + value.Director + '</td></tr>');
-                    })
-            })
-    }
-    document.getElementById("movieTable").innerHTML = makeTable();
-
-
-
 })(jQuery);
+
+function edit() {
+    var tble = document.getElementById("movieTable");
+    for (var i = 0, row; row = tble.rows[i]; i++) {
+        var movie = {
+            Id: $("#id"+i+"").val(),
+            Title: $("#tlt"+i+"").val(),
+            Genre: $("#gnr"+i+"").val(),
+            Director: $("#dir"+i+"").val()
+        }
+        ajax('https://localhost:44330/api/movies/'+movie.Id, 'put', JSON.stringify(movie),
+        function (data, textStatus, jqXhr){
+        })
+    }
+}
+
+function makeTable() {
+    ajax('https://localhost:44330/api/movies/', 'get', null,
+
+        function (data, textStatus, jQxhr) {
+            $.each(data,
+
+                function (i, value) {
+                    table.append('<tr><td style="display:none;"><input type="text" id="id'+i+'" name="id" value="' + value.Id + '" /><td><input type="text" id="tlt'+i+'" name="title" value="' + value.Title + '" /></td><td><input type="text" id="gnr'+i+'" name="genre" value="' + value.Genre + '"></td><td><input type="text" id="dir'+i+'" name="director" value="' + value.Director + '"></td></tr>');
+                })
+        })
+}
+
+makeTable();
